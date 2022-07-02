b7cae5ecd0ef7415356b653f926d0e02504ea26a
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-07-02 10:30:34 +0200

    Fixing forge thingies but it still doesn't work :weary:

--- forge/src/main/java/org/valkyrienskies/eureka/forge/EurekaModForge.java
+++ forge/src/main/java/org/valkyrienskies/eureka/forge/EurekaModForge.java
@@ -2,6 +2,7 @@ package org.valkyrienskies.eureka.forge;
 
 import me.shedaniel.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.valkyrienskies.eureka.EurekaMod;
 
@@ -10,6 +11,11 @@ public class EurekaModForge {
     public EurekaModForge() {
         // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(EurekaMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
         EurekaMod.init();
+    }
+
+    void clientSetup(FMLClientSetupEvent event) {
+        EurekaMod.initClient();
     }
 }
