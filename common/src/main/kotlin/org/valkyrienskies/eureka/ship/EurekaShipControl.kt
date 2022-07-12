4a9aed219da479f58247eec16699f5d115a33718
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-07-12 14:35:33 +0200

    Forces synced now with new api

--- common/src/main/kotlin/org/valkyrienskies/eureka/ship/EurekaShipControl.kt
+++ common/src/main/kotlin/org/valkyrienskies/eureka/ship/EurekaShipControl.kt
@@ -1,39 +1,37 @@
 package org.valkyrienskies.eureka.ship
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import org.joml.Vector3d
+import org.joml.Vector3dc
 import org.valkyrienskies.core.api.ForcesApplier
-import org.valkyrienskies.core.api.Ship
 import org.valkyrienskies.core.api.ShipForcesInducer
-import org.valkyrienskies.core.api.ShipUser
-import org.valkyrienskies.core.game.ships.ShipObjectServer
+import org.valkyrienskies.core.game.ships.PhysShip
 
 private const val STABILIZATION_TORQUE_CONSTANT = 7.5
 private const val RISE_ACC = 0.1
 private const val MAX_RISE_ACC = 2.0
 
-class EurekaShipControl : ShipForcesInducer, ShipUser {
+class EurekaShipControl : ShipForcesInducer {
 
-    @JsonIgnore
-    override var ship: Ship? = null
     var alleviationTarget: Double = 30.0
 
-    override fun applyForces(forcesApplier: ForcesApplier) {
-        val shipData = (ship as ShipObjectServer).shipData
-        val mass = shipData.inertiaData.getShipMass()
+    override fun applyForces(forcesApplier: ForcesApplier, ship: PhysShip) {
+        val mass = ship.inertia.shipMass
+        val moiTensor = ship.inertia.momentOfInertiaTensor
         val shipUp = Vector3d(0.0, 1.0, 0.0)
         val worldUp = Vector3d(0.0, 1.0, 0.0)
 
-        shipData.shipTransform.shipToWorldMatrix.transformDirection(shipUp)
+        ship.rotation.transform(shipUp)
 
         val angleBetween = shipUp.angle(worldUp)
         if (angleBetween > .01) {
             val stabilizationRotationAxisNormalized = shipUp.cross(worldUp, worldUp).normalize()
-            val stabilizationTorque = shipData.inertiaData.getMomentOfInertiaTensor()
-                .transform(stabilizationRotationAxisNormalized.mul(angleBetween, Vector3d()))
+            val idealAngularAcceleration: Vector3dc = stabilizationRotationAxisNormalized.mul(angleBetween, Vector3d())
+
+            val stabilizationTorque = ship.rotation.transform(
+                moiTensor.transform(ship.rotation.transformInverse(idealAngularAcceleration, Vector3d()))
+            )
             // stabilizationTorque.mul(1.0 / 60.0)
             stabilizationTorque.mul(STABILIZATION_TORQUE_CONSTANT)
-
             forcesApplier.applyInvariantTorque(stabilizationTorque)
         }
 
