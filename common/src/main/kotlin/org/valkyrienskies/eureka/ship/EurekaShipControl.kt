8b63eb64a37b752744622a042cb12652594fbecb
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-07-15 14:14:32 +0200

    Driving Changes to be continued

--- common/src/main/kotlin/org/valkyrienskies/eureka/ship/EurekaShipControl.kt
+++ common/src/main/kotlin/org/valkyrienskies/eureka/ship/EurekaShipControl.kt
@@ -15,6 +15,7 @@ class EurekaShipControl : ShipForcesInducer {
 
     var leftImpulse = 0.0f
     var forwardImpulse = 0.0f
+    var speedTarget = 0.0f
     var maxSpeed = 100.0f
     var alleviationTarget = 30.0
 
@@ -22,40 +23,66 @@ class EurekaShipControl : ShipForcesInducer {
         val mass = ship.inertia.shipMass
         val moiTensor = ship.inertia.momentOfInertiaTensor
         val shipUp = Vector3d(0.0, 1.0, 0.0)
+        val shipFront = Vector3d(0.0, 0.0, 1.0)
         val worldUp = Vector3d(0.0, 1.0, 0.0)
 
         ship.rotation.transform(shipUp)
+        ship.rotation.transform(shipFront)
 
-        val angleBetween = shipUp.angle(worldUp)
-        if (angleBetween > .01) {
-            val stabilizationRotationAxisNormalized = shipUp.cross(worldUp, worldUp).normalize()
-            val idealAngularAcceleration =
-                stabilizationRotationAxisNormalized.mul(angleBetween, stabilizationRotationAxisNormalized)
-            // Only subtract the x/z components of omega. We still want to allow rotation along the Y-axis (yaw).
-            idealAngularAcceleration.sub(ship.omega.x(), 0.0, ship.omega.z())
+        run { // Stabilization
+            val angleBetween = shipUp.angle(worldUp)
+            if (angleBetween > .01) {
+                val stabilizationRotationAxisNormalized = shipUp.cross(worldUp, Vector3d()).normalize()
+                val idealAngularAcceleration =
+                    stabilizationRotationAxisNormalized.mul(angleBetween, stabilizationRotationAxisNormalized)
+                // Only subtract the x/z components of omega. We still want to allow rotation along the Y-axis (yaw).
+                idealAngularAcceleration.sub(
+                    ship.omega.x(),
+                    0.0,
+                    ship.omega.z()
+                )
 
-            val stabilizationTorque = ship.rotation.transform(
-                moiTensor.transform(
-                    ship.rotation.transformInverse(
-                        idealAngularAcceleration,
-                        idealAngularAcceleration
+                val stabilizationTorque = ship.rotation.transform(
+                    moiTensor.transform(
+                        ship.rotation.transformInverse(
+                            idealAngularAcceleration,
+                            idealAngularAcceleration
+                        )
                     )
                 )
-            )
-            // stabilizationTorque.mul(1.0 / 60.0)
-            stabilizationTorque.mul(STABILIZATION_TORQUE_CONSTANT)
-            forcesApplier.applyInvariantTorque(stabilizationTorque)
+                // stabilizationTorque.mul(1.0 / 60.0)
+                stabilizationTorque.mul(STABILIZATION_TORQUE_CONSTANT)
+                forcesApplier.applyInvariantTorque(stabilizationTorque)
+            }
         }
+        // Player controlled rotation
+        val rotationVector = Vector3d(
+            0.0,
+            if (leftImpulse != 0.0f)
+                leftImpulse.toDouble() * TURN_SPEED
+            else
+                -ship.omega.y() * STABILIZATION_TORQUE_CONSTANT,
+            0.0
+        )
+        ship.rotation.transform(
+            moiTensor.transform(
+                ship.rotation.transformInverse(
+                    rotationVector,
+                    rotationVector
+                )
+            )
+        )
+
+        forcesApplier.applyInvariantTorque(rotationVector)
 
         if (alleviationTarget != Double.NaN) {
         }
 
         // forcesApplier.applyInvariantForce(Vector3d(0.0, mass * 10, 0.0))
 
+        // Player Controlled Forward
         val forceMul = clamp(0.0, 0.1, 1 - (ship.velocity.length() / maxSpeed)) * 10.0
         val forwardPower = mass * 10 * forwardImpulse * forceMul
         forcesApplier.applyRotDependentForce(Vector3d(0.0, 0.0, forwardPower))
-
-        forcesApplier.applyInvariantTorque(moiTensor.transform(Vector3d(0.0, leftImpulse.toDouble() * TURN_SPEED, 0.0)))
     }
 }
