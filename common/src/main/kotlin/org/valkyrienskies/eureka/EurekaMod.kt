cb2762474a3fbfaf5644e9fa534b0d95f48444bd
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-07-25 09:18:54 +0200

    Cleaning some code

--- common/src/main/kotlin/org/valkyrienskies/eureka/EurekaMod.kt
+++ common/src/main/kotlin/org/valkyrienskies/eureka/EurekaMod.kt
@@ -1,16 +1,7 @@
 package org.valkyrienskies.eureka
 
-import me.shedaniel.architectury.registry.Registries
-import net.minecraft.util.LazyLoadedValue
-
 object EurekaMod {
     const val MOD_ID = "vs_eureka"
-
-    val REGISTRIES = LazyLoadedValue {
-        Registries.get(
-            MOD_ID
-        )
-    }
 
     @JvmStatic
     fun init() {
