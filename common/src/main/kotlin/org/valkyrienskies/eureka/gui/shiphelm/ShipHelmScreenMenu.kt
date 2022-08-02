02c5f535dc6670e97ab86643075f19cafc26122a
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-08-02 16:30:48 +0200

    Fixes the full on broke shit; #7 still not fixed

--- common/src/main/kotlin/org/valkyrienskies/eureka/gui/shiphelm/ShipHelmScreenMenu.kt
+++ common/src/main/kotlin/org/valkyrienskies/eureka/gui/shiphelm/ShipHelmScreenMenu.kt
@@ -6,7 +6,7 @@ import net.minecraft.world.inventory.AbstractContainerMenu
 import org.valkyrienskies.eureka.EurekaScreens
 import org.valkyrienskies.eureka.blockentity.ShipHelmBlockEntity
 
-class ShipHelmScreenMenu(syncId: Int, playerInv: Inventory, var blockEntity: ShipHelmBlockEntity?) :
+class ShipHelmScreenMenu(syncId: Int, playerInv: Inventory, val blockEntity: ShipHelmBlockEntity?) :
     AbstractContainerMenu(EurekaScreens.SHIP_HELM.get(), syncId) {
 
     constructor(syncId: Int, playerInv: Inventory) : this(syncId, playerInv, null)
@@ -19,13 +19,13 @@ class ShipHelmScreenMenu(syncId: Int, playerInv: Inventory, var blockEntity: Shi
         if (blockEntity == null) return false
 
         if (id == 0 && !assembled && !player.level.isClientSide) {
-            blockEntity = blockEntity!!.assemble() ?: throw IllegalStateException()
-            // player.teleportTo(player.x, player.y + 3, player.z)
+            val blockEntity = blockEntity.assemble() // ?: throw IllegalStateException()
+            // blockEntity.sit(player, true)
             return true
         }
 
         if (id == 1 && assembled && !player.level.isClientSide) {
-            blockEntity!!.align()
+            blockEntity.align()
             return true
         }
 
