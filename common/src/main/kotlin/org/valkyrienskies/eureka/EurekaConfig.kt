7afc0f67d56499f8367872d2244a5abb8786f476
Grant Voelker <advanceseven@gmail.com>
2022-09-26 19:44:09 -0400

    - JsonSchema'd the config

--- common/src/main/kotlin/org/valkyrienskies/eureka/EurekaConfig.kt
+++ common/src/main/kotlin/org/valkyrienskies/eureka/EurekaConfig.kt
@@ -22,35 +22,45 @@ object EurekaConfig {
         var maxFloaterBuoyantFactor = 1.0
 
         // The velocity any ship at least can move at.
+        @JsonSchema(description = "The speed a ship with no engines can move at")
         var baseSpeed = 3.0
 
         // Sensitivity of the up/down impulse buttons.
         // TODO maybe should be moved to VS2 client-side config?
+        @JsonSchema(description = "Vertical sensitivity up ascend/descend")
         var impulseAlleviationRate = 0.5
 
         // If a ship with weight 0 and 0 balloons would exist in the world, it would have this max attitude.
+        @JsonSchema(description = "The Y level that a ship with 0 mass would naturally float to")
         var neutralLimit = 80.0
 
         // Do i need to explain? the mass 1 baloon gets to float
+        @JsonSchema(description = "Amount of mass in kg a balloon can lift")
         var massPerBalloon = 5000.0
 
         // The amount of speed that the ship can move at when the left/right impulse button is held down.
+        @JsonSchema(description = "Turn sensitivity of the ship helm")
         var turnSpeed = 3.0
 
         // The strength used when trying to level the ship
+        @JsonSchema(description = "How much torque a ship will apply to try and keep level")
         var stabilizationTorqueConstant = 15.0
 
         // Max anti-velocity used when trying to stop the ship
+        @JsonSchema(description = "How fast a ship will stop. 1 = fast stop, 0 = slow stop")
         var linearStabilizeMaxAntiVelocity = 1.0
 
         // Anti-velocity mass relevance when stopping the ship
         // Max 10.0 (means no mass irrelevance)
+        @JsonSchema(description = "How much inertia affects Eureka ships. Max 10 = full inertia")
         var antiVelocityMassRelevance = 0.8
 
         // Chance that if side will pop, its this chance per side
+        @JsonSchema(description = "Chance for popped balloons to pop adjacent balloons, per side")
         var popSideBalloonChance = 0.3
 
         // Blacklist of blocks that don't get added for ship building
+        @JsonSchema(description = "Blacklist of blocks that don't get assembled")
         var blockBlacklist = setOf(
             "vs_eureka:ship_helm",
             "minecraft:dirt",
@@ -131,7 +141,10 @@ object EurekaConfig {
             "minecraft:portal"
         )
 
+        @JsonSchema(description = "Whether the ship helm assembles diagonally connected blocks or not")
         val diagonals = true
+
+        @JsonSchema(description = "How many blocks to assemble per tick")
         val assembliesPerTick = 1000
 
         @JsonSchema(description = "Weight of ballast when lowest redstone power")
