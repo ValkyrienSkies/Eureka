b5ddc22b37e12fdab3c79b37d8344eb262813bca
Rubydesic <rubydesic@gmail.com>
2022-11-28 19:41:17 -0500

    update to latest VS2, reinstate dependency downloader

--- forge/src/main/java/org/valkyrienskies/eureka/forge/AutoDependenciesForge.java
+++ forge/src/main/java/org/valkyrienskies/eureka/forge/AutoDependenciesForge.java
@@ -1,13 +1,18 @@
 package org.valkyrienskies.eureka.forge;
 
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.loading.FMLPaths;
+import org.valkyrienskies.dependency_downloader.ValkyrienDependencyDownloader;
+
 public class AutoDependenciesForge {
     public static void runUpdater() {
-//        boolean isServer = FMLEnvironment.dist.isDedicatedServer();
-//
-//        ValkyrienDependencyDownloader.start(
-//            FMLPaths.MODSDIR.get(),
-//            FMLLoader.getLoadingModList().getModFileById("vs_eureka").getFile().getFilePath(),
-//            isServer
-//        );
+        boolean isServer = FMLEnvironment.dist.isDedicatedServer();
+
+        ValkyrienDependencyDownloader.start(
+            FMLPaths.MODSDIR.get(),
+            FMLLoader.getLoadingModList().getModFileById("vs_eureka").getFile().getFilePath(),
+            isServer
+        );
     }
 }
