70384be1ec19d5d71a153fa32f7c491c4a2d6d29
StewStrong <stewstronger@gmail.com>
2025-05-31 10:52:56 -0700

    Fix compile errors, enable GitHub actions for 1.21.1

--- common/src/main/kotlin/org/valkyrienskies/eureka/blockentity/ShipHelmBlockEntity.kt
+++ common/src/main/kotlin/org/valkyrienskies/eureka/blockentity/ShipHelmBlockEntity.kt
@@ -38,8 +38,8 @@ import org.valkyrienskies.mod.common.util.toDoubles
 import org.valkyrienskies.mod.common.util.toJOMLD
 import org.valkyrienskies.mod.util.logger
 
- var ASSEMBLE_BLACKLIST: TagKey<Block> =
-     TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(EurekaMod.MOD_ID, "assemble_blacklist"))
+val ASSEMBLE_BLACKLIST: TagKey<Block> =
+    TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(EurekaMod.MOD_ID, "assemble_blacklist"))
 
 class ShipHelmBlockEntity(pos: BlockPos, state: BlockState) :
     BlockEntity(EurekaBlockEntities.SHIP_HELM.get(), pos, state), MenuProvider {
