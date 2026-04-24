07cf4181d72590731d140d603bda5c9de23c5ae7
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2026-04-24 20:57:16 +0200

    Update vs2 dependency from 2.4.5 to 2.4.10

--- common/src/main/kotlin/org/valkyrienskies/eureka/util/ShipAssembler.kt
+++ common/src/main/kotlin/org/valkyrienskies/eureka/util/ShipAssembler.kt
@@ -35,7 +35,7 @@ object ShipAssembler {
         blocks.add(center)
         val result = bfs(level, center, blocks, predicate)
         return if (result) {
-            assembleToShip(level, blocks)
+            assembleToShip(level, blocks.toSet())
         } else {
             null
         }
